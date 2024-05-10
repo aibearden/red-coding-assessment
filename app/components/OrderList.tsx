fe3017131b53e697c0ade60ff356862bae4270db
Austin Bearden <austinb99@4jlt.com>
2024-05-10 16:21:11 -0400

    Successfully added a simple unit test for TableActionBar component

--- app/components/OrderList.tsx
+++ app/components/OrderList.tsx
@@ -11,29 +11,25 @@ interface ItemListInterface {
     setRowSelectionModel: any
 }
 
+export const OrderListTableColumns: GridColDef[] = [
+    {field: "orderId", headerName: "Order ID", width: 300},
+    {field: "createdDate", headerName: "Creation Date", width: 200},
+    {field: "createdByUserName", headerName: "Created By", width: 200},
+    {field: "orderType", headerName: "Order Type", width: 200},
+    {field: "customerName", headerName: "Customer", width: 150},
+    {field: "actions", headerName: "", width: 50, renderCell: () => {
+        return (<IconButton size="small" onClick={() => {}}><Edit fontSize="small"/></IconButton>);
+     }}
+];
+
 export default function OrderList({orders, rowSelectionModel, setRowSelectionModel}: ItemListInterface) {
-
-    function handleEdit(evt: any, orderId: GridRowId) {
-        evt.stopPropagation();
-    }
-
-    const columns: GridColDef[] = [
-        {field: "orderId", headerName: "Order ID", width: 300},
-        {field: "createdDate", headerName: "Creation Date", width: 200},
-        {field: "createdByUserName", headerName: "Created By", width: 200},
-        {field: "orderType", headerName: "Order Type", width: 200},
-        {field: "customerName", headerName: "Customer", width: 150},
-        {field: "actions", headerName: "", width: 50, renderCell: (params) => {
-            return (<IconButton size="small" onClick={(evt: any) => handleEdit(evt, params.id)}><Edit fontSize="small"/></IconButton>);
-         }}
-    ];
 
     return (
         <Box sx={{height: 400, width: '100%'}} id={"orders-list"}>
             <DataGrid
                 density="compact"
                 rows={orders}
-                columns={columns}
+                columns={OrderListTableColumns}
                 getRowId={(row) => row.orderId}
                 checkboxSelection
                 hideFooter
---
+++ app/tests/TableActionBar.test.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import {render, screen} from '@testing-library/react';
+import TableActionBar from '../components/TableActionBar';
+import "./setupTests";
+import { store } from '../store/store';
+import { Provider } from 'react-redux';
+
+test('Succesfully loads and renders TableActionBar component', () => {
+
+    const tableActionBar = render(<Provider store={store}><TableActionBar refreshOrderList={() => {}} searchOrders={() => {}} rowSelectionModel={[]} setRowSelectionModel={() => {}} filteredOrderTypes={[]} setFilteredOrderTypes={() => {}}/></Provider>)
+
+    expect(tableActionBar.getAllByText('Order Type'));
+})
