f13c809958d5c4210fdb8a2cce09db63335f076b
Austin Bearden <austinb99@4jlt.com>
2024-05-10 18:07:37 -0400

    Updated TableActionBar test to include relevant filteredOrderType value

--- app/tests/TableActionBar.test.tsx
+++ app/tests/TableActionBar.test.tsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 
 test('Succesfully loads and renders TableActionBar component', () => {
 
-    const tableActionBar = render(<Provider store={store}><TableActionBar refreshOrderList={() => {}} searchOrders={() => {}} rowSelectionModel={[]} setRowSelectionModel={() => {}} filteredOrderType={""} setFilteredOrderType={() => {}}/></Provider>)
+    const tableActionBar = render(<Provider store={store}><TableActionBar refreshOrderList={() => {}} searchOrders={() => {}} rowSelectionModel={[]} setRowSelectionModel={() => {}} filteredOrderType={"Standard"} setFilteredOrderType={() => {}}/></Provider>)
 
     expect(tableActionBar.getAllByText('Order Type'));
 })
